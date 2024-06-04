1f01e4d518fb93828ac987e515d244e4cc4522f4
GeorgeTech20 <giorgifrancknr@gmail.com>
2024-06-04 03:08:45 -0500

    practica-2

---
+++ Codigo/practica-2/practica-2/practica-2/Controllers/RegistrarController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using practica_2.Models;
+
+namespace practica_2.Controllers
+{
+    public class RegistrarController : Controller
+    {
+        public ActionResult ActiontRgistrar() 
+        {
+            return View(new Registro());
+        }
+
+        [HttpPost] public ActionResult ActiontRgistrar(Registro reg) 
+        {
+            ViewBag.Costo = reg.Costo().ToString("####.#0");
+            ViewBag.Seguro = reg.Seguro().ToString("####.#0");
+            ViewBag.Monto = reg.Monto().ToString("####.#0");
+            ViewBag.IGV = reg.IGV().ToString("####.#0");
+            ViewBag.MontoTotal = reg.MontoTotal().ToString("####.#0");
+            return View(reg);
+        }
+    }
+}
---
+++ Codigo/practica-2/practica-2/practica-2/Models/Registro.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace practica_2.Models
+{
+    public class Registro
+    {
+        public string numero { get; set; }
+        public DateTime fecha { get; set; }
+
+        public string nombre { get; set; }
+
+        public string descripcion { get; set; }
+
+        public double precio { get; }
+
+        public double tm { get; set; }
+
+        public Registro()
+        {
+            numero = "";
+            fecha = DateTime.Today;
+            nombre = "";
+            descripcion = "";
+            precio = 150 ;
+            tm = 0 ;
+        }
+
+        //metodos
+
+        public double Costo() { return precio * tm; }
+        public double Seguro() { return Costo() * 0.02;  }
+
+        public double Monto() { return Costo() + Seguro(); }
+
+        public double IGV() { return Monto() * 0.18; }
+
+        public double MontoTotal() { return Monto() * IGV(); }
+
+
+
+    }
+}
